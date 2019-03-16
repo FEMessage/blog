88145b508f3e98298e7db993521e02b9d1db5235
levy <897895407@qq.com>
2019-03-16 15:09:55 +0800

    add base

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,14 +1,15 @@
 require('dotenv').config()
 
 module.exports = {
- // 站点配置
- plugins: [
-   [
-     'vuepress-plugin-yuque', {
-		   html: true,
-       repoUrl: 'https://www.yuque.com/deepexi-company/blog-in130r',
-			 authToken: process.env.TOKEN
-     }
-   ]
- ]
+  base: '/blog/',
+  // 站点配置
+  plugins: [
+    [
+      'vuepress-plugin-yuque', {
+   	   html: true,
+        repoUrl: 'https://www.yuque.com/deepexi-company/blog-in130r',
+   		 authToken: process.env.TOKEN
+      }
+    ]
+  ]
 } 
