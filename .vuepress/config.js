ff68423886e171d3b428f04068179b8482529318
轻剑快马 <xiaoranran1993@outlook.com>
2019-05-20 19:08:50 +0800

    docs:  添加代码注释

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -5,9 +5,10 @@ const path = require('path')
 const fg = require('fast-glob')
 const chokidar = require('chokidar')
 
-const patterns = ['post/**/*.md']
+const patterns = ['post/**/*.md'] // 只查询 post 目录
 const posts = new Set(fg.sync(patterns))
 
+// 生成 _sidebar 数组
 function generateSidebar() {
   const _sidebar = []
   const folders = fs.readdirSync('post', { encoding: 'utf-8' })
@@ -24,6 +25,7 @@ function generateSidebar() {
     const name = path.basename(file, '.md')
 
     _sidebar.forEach(item => {
+      // 如果文件名以 '_' 开头说明是侧边栏的标题文件
       if (name.startsWith('_') && file.includes(item.folder)) {
         item.title = fs.readFileSync(file, { encoding: 'utf-8' }).trim()
       } else if (file.includes(item.folder)) {
@@ -36,7 +38,7 @@ function generateSidebar() {
 }
 
 let sidebar = generateSidebar()
-const watcher = chokidar.watch(patterns, { ignoreInitial: true })
+const watcher = chokidar.watch(patterns, { ignoreInitial: true }) // 创建监听文件变化的观察者
 
 watcher
   .on('add', file => {
@@ -72,7 +74,7 @@ module.exports = {
     repo: 'FEMessage/blog',
     editLinks: true,
     editLinkText: '帮助我们完善此页面',
-    sidebar
+    sidebar // 传入配置项
   },
   // 站点配置
   plugins: []
